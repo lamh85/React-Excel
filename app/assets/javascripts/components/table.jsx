c53d3798fbd9605cdc6ff8bea213aeb115af5b9e
Hon Lam <lamh85@gmail.com>
2016-03-01 19:33:20 -0800

    Learning about React props.

--- app/assets/javascripts/components/table.jsx
+++ app/assets/javascripts/components/table.jsx
@@ -2,8 +2,20 @@ var Table = React.createClass({
   render: function(){
     return (
       <div>
-        Hello from component
+        <h1>The name of this table is {this.props.name}</h1>
+      </div>
+
+      <Row />
+    )
+  }
+});
+
+var Row = React.createClass({
+  render: function(){
+    return (
+      <div>
+        I am from the "row" component
       </div>
     )
   }
-})
+});
