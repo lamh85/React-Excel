6955bf9387d7a165d298f16e49f3dc8b9eedd074
Hon Lam <lamh85@gmail.com>
2016-03-02 18:27:10 -0800

    Added CellHeader component.

--- app/assets/javascripts/components/table.jsx
+++ app/assets/javascripts/components/table.jsx
@@ -3,18 +3,28 @@ var Table = React.createClass({
     return (
       <div>
         <h1>The name of this table is {this.props.name}</h1>
+        <Row name="John Doe"></Row>
+        <Row name="Jane Roe"></Rov>
       </div>
-
-      <Row />
     )
   }
 });
-
+ 
 var Row = React.createClass({
   render: function(){
     return (
       <div>
-        I am from the "row" component
+        I am from the row component. My name is {this.props.name}
+      </div>
+    )
+  }
+});
+
+var CellHeader = React.createClass({
+  render: function(){
+    return (
+      <div>
+        {this.props.name}
       </div>
     )
   }
